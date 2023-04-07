ecdcb644244f38f2e89da5e12983eddfb44f8b48
Nimap <Nimap@DESKTOP-RRJ6NCT>
2023-04-07 18:34:07 +0530

    Add project files.

---
+++ Day2/IfElse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class IfElse
+    {
+        public static void Main2(string[] args)
+        {
+            Console.WriteLine("What is your AGE?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 18)
+            {
+                Console.WriteLine("You are not Eligible for license");
+            }
+            else
+            {
+                Console.WriteLine("You are Eligible for License");
+            }
+        }
+    }
+}
---
+++ Day2/NestedIf.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class NestedIf
+    {
+        public static void Main4(string[] args)
+        {
+            int a = 34; int b = 54; int c = 301; int d = 58;
+
+            if (a > b && a>c && a>d)
+            {
+                Console.WriteLine("A is large than all");
+            }
+            else
+            {
+                if(b>c && b > d)
+                {
+                    Console.WriteLine("B is large than all");
+                }
+                else
+                {
+                    if (c > d)
+                    {
+                        Console.WriteLine("C is large than all");
+                    }
+                    else
+                    {
+                        Console.WriteLine("D is large than all");
+                    }
+                }
+            }
+            
+        }
+    }
+}
